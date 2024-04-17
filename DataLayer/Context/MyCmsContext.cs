04761a8b8f3d2fefa21ebadb9a3f798ab8fe65b9
SeyedReza <s.tabaeeyan@gmail.com>
2024-04-17 19:15:04 +0330

    Create Context

---
+++ DataLayer/Context/MyCmsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context
+{
+    public class MyCmsContext:DbContext
+    {
+        public DbSet<PageGroup> PageGroups { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<PageComment> pageComments { get; set; }
+    }
+}
