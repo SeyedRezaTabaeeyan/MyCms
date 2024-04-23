e745d0ba3c9e8b35829c1bd76d078f258dbe22cd
SeyedReza <s.tabaeeyan@gmail.com>
2024-04-23 12:07:41 +0330

    Convert Field CreateDate from Miladi To Shamsi

---
+++ MyCms/Classes/PersianDateConvertor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace MyCms
+{
+    public static class PersianDateConvertor
+    {
+        public static string ToShamsi(this DateTime date)
+        {
+            PersianCalendar pc=new PersianCalendar();
+            return pc.GetYear(date) + "/" + pc.GetMonth(date).ToString("00")  + "/" + pc.GetDayOfMonth(date).ToString("00");
+        }
+    }
+}
